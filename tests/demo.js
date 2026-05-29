ec96e56581e722d11799a9bbb9f77f069ae2a391
Ramya <ramya.alapati1@gmail.com>
2026-05-29 16:45:19 +0800

    second commit

--- tests/demo.js
+++
@@ -1,9 +0,0 @@
-const { connected } = require("node:process");
-
-let message = "Hello Message";
-message = 2;
-console.log(message);
-
-let numbers = [1, 2, 3];
-console.log(numbers);
-console.log(numbers[1]);
